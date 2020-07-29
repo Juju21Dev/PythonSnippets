c97e5231979f6c3441de5c47c371dc683c886145
Julien Violette <54059934+Juju21Dev@users.noreply.github.com>
2020-07-29 15:36:01 +0200

    Add files via upload

---
+++ geometricSequence.py
@@ -0,0 +1,22 @@
+# Program Geometric Sequence
+# terms of a geometric sequence
+# date : 19/07/2020
+# author : Julien Violette
+
+# keyboarding of the first term, common ratio and number of terms
+a = int(input("Enter the first term of the sequence = "))
+r = int(input("Enter the common ratio of the sequence = "))
+nb = int(input("Enter the number of the terms = "))
+
+# initializations
+term = a
+sum = 0
+
+# loop that displays the nb first terms of the sequence
+for k in range(0, nb):
+    print(f"u{k} = {term}")
+    sum += term
+    term *= r
+
+# displays the sum
+print(f"the sum of the sequence's first terms = {sum}")
