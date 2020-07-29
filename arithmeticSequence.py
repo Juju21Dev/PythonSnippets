8690de139618d5a3409e467533c2ebd895584433
Julien Violette <54059934+Juju21Dev@users.noreply.github.com>
2020-07-29 15:27:24 +0200

    Add files via upload

---
+++ arithmeticSequence.py
@@ -0,0 +1,24 @@
+# Program Arithmetic Sequence
+# terms of an arithmetic sequence
+# date : 19/07/2020
+# author : Julien Violette
+
+# keyboarding of the first term, common difference and number of terms
+a = int(input("Enter the first term of the sequence = "))
+d = int(input("Enter the common difference of the sequence = "))
+nb = int(input("Enter the number of terms = "))
+
+# initialization
+term = a
+
+# initialization of the sum
+sum = 0
+
+# loop that displays the nb first terms of the sequence
+for k in range(0, nb):
+    print(f"u{k} = {term}")
+    sum += term
+    term += d
+
+# displays the sum
+print(f"the sum of the sequence's first terms = {sum}")
